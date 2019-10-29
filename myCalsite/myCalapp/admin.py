816513aa3b3db6d6c3d873ad3a31b155a168dc26
Makena <maklh899@hotmail.com>
2019-10-29 09:58:25 -0700

    built models

--- myCalsite/myCalapp/admin.py
+++ myCalsite/myCalapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Event_user)
+admin.site.register(models.Event_group)
+admin.site.register(models.Task_user)
+admin.site.register(models.Task_group)
