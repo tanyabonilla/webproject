f797e006f255d6036b02531bd84a3a225f02d186
maklh899 <maklh899@hotmail.com>
2019-12-16 16:27:19 -0800

    added json

--- myCalsite/myCalapp/urls.py
+++ myCalsite/myCalapp/urls.py
@@ -9,11 +9,15 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view()),
     path('register/', views.register),
     path('myfriends/', views.add_remove_friend),
+    path('myfriends/friends/', views.friends_view), #json
     path('chat/', views.chatindex, name='chat'),
     path('chat/<str:room_name>/', views.room, name='room'),
-    path('new_event/', views.index), #new_event
-    path('month/', views.index), #monthly
-    # path('events/', views.events_view),
-    # path('tasks/', views.tasks_view),
-    
+    path('new_events_tasks/', views.new_events_tasks), #new_event
+    #path('month/', views.index), #monthly
+    path('events/', views.events_view), #json
+    path('new_events_tasks/events/', views.events_view), #json
+    path('tasks/', views.tasks_view), #json
+    path('new_events_tasks/tasks/', views.tasks_view), #json
+    path('friends/', views.friends_view), #json
+
 ]
